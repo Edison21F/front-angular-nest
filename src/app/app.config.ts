bba93630fec7f32385e592c9b7e0ed3fa25822de
Edison20F <ela.flores@yaviac.edu.ec>
2024-12-08 21:25:47 -0500

    Proyeto base

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), // Configuración de rutas
+    provideClientHydration(), // Soporte para Hydration si usas SSR
+    importProvidersFrom(BrowserModule), // Importar BrowserModule
+    importProvidersFrom(BrowserAnimationsModule), // Importar BrowserAnimationsModule
+    provideHttpClient(withFetch()), // Configurar HttpClient con fetch
+    provideZoneChangeDetection({ eventCoalescing: true }) // Optimización del cambio de zonas
+  ]
 };
