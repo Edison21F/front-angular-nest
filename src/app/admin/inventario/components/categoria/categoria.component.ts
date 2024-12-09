84ee45dc6a30dd8e9d2f1b9e9c8d10fc4a8b446d
Edison20F <ela.flores@yaviac.edu.ec>
2024-12-08 22:35:25 -0500

    guardado_categoria

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoriaService } from '../../services/categoria.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
+interface Categoria {
+  id?: number;
+  nombre: string;
+  detalle?:string;
+}
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
-export class CategoriaComponent {
+export class CategoriaComponent implements OnInit {
+
+  private categoriaService = inject(CategoriaService)
+  categorias: Categoria[] = [];
+  visible:boolean=false;
+  categoriaForm= new FormGroup({
+    nombre: new FormControl(''),
+    detalle: new FormControl('')
+  });
+
+  ngOnInit(): void {
+      this.getCategoria()
+  }
+  getCategoria(){
+    this.categoriaService.funListar().subscribe(
+      (res:any)=>{
+        this.categorias = res;
+      },
+      (err:any)=>{
+        console.log(err);
+      }
+    )
+  }
+
+  mostrarDialog(){
+    this.visible=true
+  }
+
+  guardarCategoria(){
+    if (this.categoriaForm.valid) { // Verificar si el formulario es válido
+      this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
+        (res:any) => {
+          this.visible = false;
+          this.getCategoria(); // Recargar las categorías después de guardar
+        },
+        (err:any) => {
+          console.error('Error al guardar la categoría', err);
+        }
+      );
+    } else {
+      console.warn('Formulario inválido');
+    }
+  }
+  
 
 }
